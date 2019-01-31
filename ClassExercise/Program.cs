f8e6b789b76e6d6dab70fb843fafe2676a4f9983
Jared Hanlon <jaredhanlon@mail.usf.edu>
2019-01-31 18:01:59 -0500

    version 2

--- ClassExercise/Program.cs
+++ ClassExercise/Program.cs
@@ -6,7 +6,27 @@ namespace ClassExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+            Console.WriteLine("Please enter your name: ");
+            try
+            {
+                string input = Console.ReadLine();
+                Recommend_Name(input);
+            } // End of try
+            catch
+            {
+                Console.WriteLine("There was a problem processing that input...");
+                Console.WriteLine("Please press any key to exit the program and try again...");
+                // Pause the program and await the user to press a key to end the program
+                Console.ReadKey(true);
+            } // End of catch
+        } // End of main
+        private static void Recommend_Name(string get_name)
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("Hello " + get_name + "!");
+            Console.ReadKey(true);
+        } // End of Recommend_Name
+    } // End of class
+   } // End of namespace
+
+
